58e86a896ca6ac00c13b5dc0ab5e002b6343cb69
Your Name <you@example.com>
2022-12-16 19:04:17 +0530

    level 3 to-do list using closure

--- index.js
+++ index.js
@@ -1,81 +1,143 @@
 const todoList = () => {
   all = [];
-  const add = (todoTask) => {
-    all.push(todoTask);
-    console.log(all);
+  const add = (todoItem) => {
+    all.push(todoItem);
   };
   const markAsComplete = (index) => {
     all[index].completed = true;
-    console.log(all);
   };
-  return { all, add, markAsComplete };
+
+  const overdue = () => {
+    // Write the date check condition here and return the array of overdue items accordingly.
+    // FILL YOUR CODE HERE
+    // ..
+    // ..
+    // ..
+    let arr = [];
+    let length = all.length;
+    for (let i = 0; i < length; i++) {
+      if (all[i].dueDate == yesterday) {
+        arr.push(all[i]);
+      }
+    }
+    return arr;
+  };
+
+  const dueToday = () => {
+    // Write the date check condition here and return the array of todo items that are due today accordingly.
+    // FILL YOUR CODE HERE
+    // ..
+    // ..
+    // ..
+    let arr = [];
+    let length = all.length;
+    for (let i = 0; i < length; i++) {
+      if (all[i].dueDate == today) {
+        arr.push(all[i]);
+      }
+    }
+    return arr;
+  };
+
+  const dueLater = () => {
+    // Write the date check condition here and return the array of todo items that are due later accordingly.
+    // FILL YOUR CODE HERE
+    // ..
+    // ..
+    // ..
+    let arr = [];
+    let length = all.length;
+    for (let i = 0; i < length; i++) {
+      if (all[i].dueDate == tomorrow) {
+        arr.push(all[i]);
+      }
+    }
+    return arr;
+  };
+
+  const toDisplayableList = (list) => {
+    // Format the To-Do list here, and return the output string as per the format given above.
+    // FILL YOUR CODE HERE
+    // ..
+    // ..
+    // ..
+    let OUTPUT_STRING = "";
+    let length = list.length;
+    for (let i = 0; i < length; i++) {
+      if (list[i].completed == false) {
+        OUTPUT_STRING = OUTPUT_STRING + "[" + " ] ";
+      } else {
+        OUTPUT_STRING = OUTPUT_STRING + "[" + "x] ";
+      }
+      if (list[i].dueDate == today) {
+        OUTPUT_STRING += list[i].title + "\n";
+      } else {
+        if (i == length - 1 && tomorrow == list[i].dueDate) {
+          OUTPUT_STRING =
+            OUTPUT_STRING + list[i].title + ". " + list[i].dueDate + "\n";
+        } else {
+          OUTPUT_STRING =
+            OUTPUT_STRING + list[i].title + " " + list[i].dueDate + "\n";
+        }
+      }
+    }
+
+    // return OUTPUT_STRING
+    return OUTPUT_STRING;
+  };
+
+  return {
+    all,
+    add,
+    markAsComplete,
+    overdue,
+    dueToday,
+    dueLater,
+    toDisplayableList,
+  };
 };
+
+// ####################################### #
+// DO NOT CHANGE ANYTHING BELOW THIS LINE. #
+// ####################################### #
+
 const todos = todoList();
-todos.add({
-  title: "I need to prepare",
-  dueDate: "05-12-2022",
-  completed: false,
-});
-todos.add({
-  title: "Renew insurance",
-  dueDate: "08-12-2022",
-  completed: false,
-});
 
-todos.markAsComplete(1);
-todos.all;
-todos.all;
-// const http = require("http");
-// const fs = require("fs");
+const formattedDate = (d) => {
+  return d.toISOString().split("T")[0];
+};
 
-// let port = require("minimist")(process.argv.slice(2));
-// port = port._;
-// port = port[0];
+var dateToday = new Date();
+const today = formattedDate(dateToday);
+const yesterday = formattedDate(
+  new Date(new Date().setDate(dateToday.getDate() - 1))
+);
+const tomorrow = formattedDate(
+  new Date(new Date().setDate(dateToday.getDate() + 1))
+);
 
-// let homeContent = "";
-// let projectContent = "";
-// let registrationContent = "";
+todos.add({ title: "Submit assignment", dueDate: yesterday, completed: false });
+todos.add({ title: "Pay rent", dueDate: today, completed: true });
+todos.add({ title: "Service Vehicle", dueDate: today, completed: false });
+todos.add({ title: "File taxes", dueDate: tomorrow, completed: false });
+todos.add({ title: "Pay electric bill", dueDate: tomorrow, completed: false });
 
-// fs.readFile("home.html", (err, home) => {
-//   if (err) {
-//     throw err;
-//   }
-//   homeContent = home;
-// });
+console.log("My Todo-list\n\n");
 
-// fs.readFile("project.html", (err, project) => {
-//   if (err) {
-//     throw err;
-//   }
-//   projectContent = project;
-// });
+console.log("Overdue");
+var overdues = todos.overdue();
+var formattedOverdues = todos.toDisplayableList(overdues);
+console.log(formattedOverdues);
+console.log("\n\n");
 
-// fs.readFile("registration.html", (err, registration) => {
-//   if (err) {
-//     throw err;
-//   }
-//   registrationContent = registration;
-// });
+console.log("Due Today");
+let itemsDueToday = todos.dueToday();
+let formattedItemsDueToday = todos.toDisplayableList(itemsDueToday);
+console.log(formattedItemsDueToday);
+console.log("\n\n");
 
-// http
-//   .createServer((request, response) => {
-//     let url = request.url;
-//     response.writeHeader(200, { "Content-Type": "text/html" });
-//     switch (url) {
-//       case "/project":
-//         response.write(projectContent);
-//         response.end();
-//         break;
-//       case "/registration":
-//         response.write(registrationContent);
-//         response.end();
-//         break;
-//       default:
-//         response.write(homeContent);
-//         response.end();
-//         break;
-//     }
-//   })
-//   .listen(5000, () => {
-//     console.log("server is listening");
-//   });
+console.log("Due Later");
+let itemsDueLater = todos.dueLater();
+let formattedItemsDueLater = todos.toDisplayableList(itemsDueLater);
+console.log(formattedItemsDueLater);
+console.log("\n\n");
