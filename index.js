f4df07b59dbad87f0fcdcb825d29be6cfbe10155
Your Name <you@example.com>
2022-12-16 08:42:00 +0530

    level 2 http server

--- index.js
+++ index.js
@@ -1,5 +1,54 @@
-function hello() {
-    console.log("Hello Node.Js!");
+const http = require("http");
+const fs = require("fs");
+
+let port = require("minimist")(process.argv.slice(2));
+port = port._;
+port = port[0];
+
+let homeContent = "";
+let projectContent = "";
+let registrationContent = "";
+
+fs.readFile("home.html", (err, home) => {
+  if (err) {
+    throw err;
   }
-  
-hello();
+  homeContent = home;
+});
+
+fs.readFile("project.html", (err, project) => {
+  if (err) {
+    throw err;
+  }
+  projectContent = project;
+});
+
+fs.readFile("registration.html", (err, registration) => {
+  if (err) {
+    throw err;
+  }
+  registrationContent = registration;
+});
+
+http
+  .createServer((request, response) => {
+    let url = request.url;
+    response.writeHeader(200, { "Content-Type": "text/html" });
+    switch (url) {
+      case "/project":
+        response.write(projectContent);
+        response.end();
+        break;
+      case "/registration":
+        response.write(registrationContent);
+        response.end();
+        break;
+      default:
+        response.write(homeContent);
+        response.end();
+        break;
+    }
+  })
+  .listen(5000, () => {
+    console.log("server is listening");
+  });
