0d458d890f0f0ccc4f9196581bee4cfb5d589f48
Your Name <you@example.com>
2022-12-19 19:36:19 +0530

    level 4 add tests for todo list

--- __tests__/todo.js
+++ __tests__/todo.js
@@ -1,13 +1,29 @@
 /* eslint-disable no-undef */
 const todoList = require("../todo");
 
-const { all, markAsComplete, add } = todoList();
+const { all, markAsComplete, add, overdue, dueToday, dueLater } = todoList();
 describe("Todolist Test Suite", () => {
   beforeAll(() => {
     add({
       title: "Test todo",
       completed: false,
       dueDate: new Date().toLocaleDateString("en-CA"),
+    });
+    var date1 = new Date();
+    date1.setDate(date1.getDate() - 1);
+    var due = date1.toLocaleDateString("en-CA");
+    var date2 = new Date();
+    date2.setDate(date2.getDate() + 1);
+    var duelater = date2.toLocaleDateString("en-CA");
+    add({
+      title: "Test todo",
+      completed: false,
+      dueDate: due,
+    });
+    add({
+      title: "Test todo",
+      completed: false,
+      dueDate: duelater,
     });
   });
   test("Should add new todo", () => {
@@ -23,5 +39,20 @@ describe("Todolist Test Suite", () => {
     expect(all[0].completed).toBe(false);
     markAsComplete(0);
     expect(all[0].completed).toBe(true);
+  });
+  test("Checks retrieval of overdue items", () => {
+    var due = overdue();
+    expect(all[1].title).toBe(due[0].title);
+    expect(all[1].dueDate).toBe(due[0].dueDate);
+  });
+  test("Checks retrieval of dueLater items", () => {
+    var later = dueLater();
+    expect(all[2].title).toBe(later[0].title);
+    expect(all[2].dueDate).toBe(later[0].dueDate);
+  });
+  test("Checks retrieval of due today items", () => {
+    var today = dueToday();
+    expect(all[0].title).toBe(today[0].title);
+    expect(all[0].dueDate).toBe(today[0].dueDate);
   });
 });
